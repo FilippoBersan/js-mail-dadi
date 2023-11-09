8df5498f39f15beb704fd45dba87989ac8b5ecbe
FilippoBersan <filippobersan99@gmail.com>
2023-11-09 14:53:27 +0100

    feat: scaffolding esercizio 1

---
+++ esercizio1/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Esercizio 1 Mail
+
+// Chiedi all’utente la sua email,
+
+// 1. creo prompt mail
+
+// 2. creo varie mail che possono accedere con array
+
+// 3. se è nella lista può accedere
+
+// controlla che sia nella lista di chi può accedere,
+
+// stampa un messaggio appropriato sull’esito del controllo.
+//  4) stampo in console
